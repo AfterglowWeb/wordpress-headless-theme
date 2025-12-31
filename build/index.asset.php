0b9db6f41b1ff9928d0e716d3aad1630fe3e3e98
Afterglow <cedric@moriskelly.com>
2025-12-31 23:18:30 +0100

    rm build

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '5bcb4b61559098184de9');
--- build/mui.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '8236f18107007bc98a78');
