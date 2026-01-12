6644a00fb6bb18d4e5a6284a044d196d45379d47
Afterglow <cedric@moriskelly.com>
2026-01-12 11:23:10 +0100

    rate limit options, ui polish wip

--- inc/Rest/RateLimiting.php
+++ inc/Rest/RateLimiting.php
@@ -2,16 +2,21 @@
 
 defined( 'ABSPATH' ) || exit;
 
+use cmk\blank\Admin;
+
 class RateLimit {
 
 	public static function check( \WP_REST_Request $request, int $limit = 60 ) {
 
 		$user_id = get_current_user_id();
 		$key     = 'blank_rl_' . md5( $user_id . $request->get_route() );
+		$admin_options = Admin::read_admin_options();
+		$rate_limit = (int) $admin_options['rest_api_rate_limit'];
+		$time_limit = (int) $admin_options['rest_api_rate_limit_time'];
 
 		$count = (int) get_transient( $key );
 
-		if ( $count >= $limit ) {
+		if ( $count >= $rate_limit ) {
 			return new \WP_Error(
 				'blank_rate_limited',
 				'Too many requests.',
@@ -19,7 +24,7 @@ class RateLimit {
 			);
 		}
 
-		set_transient( $key, $count + 1, MINUTE_IN_SECONDS );
+		set_transient( $key, $count + 1, $time_limit );
 
 		return true;
 	}
